f625f780d9c213101b5688ac6df9488c0bbf6d72
Alexey Shaytan <alex@intbio.org>
2019-01-14 22:32:34 +0300

    Update

--- plumed2_v2.5.0/build.sh
+++ plumed2_v2.5.0/build.sh
@@ -2,7 +2,7 @@
 
 export MACOSX_DEPLOYMENT_TARGET=10.9 # - helps on some OSX platforms
 
-./configure --prefix=$PREFIX LDFLAGS=-lz
+./configure --prefix=$PREFIX LDFLAGS=-L../_build_env/lib/
 make -j 4
 make install
 
