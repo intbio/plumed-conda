6823acfac1f5fa31786d7d800dafa6f599a44223
Alexey Shaytan <molsim@users.noreply.github.com>
2019-01-19 23:04:40 +0300

    Update build.sh

--- plumed2_v2.5.0/build.sh
+++ plumed2_v2.5.0/build.sh
@@ -2,7 +2,7 @@
 
 export MACOSX_DEPLOYMENT_TARGET=10.9 # - helps on some OSX platforms
 
-./configure --prefix=$PREFIX LDFLAGS=$BUILD_PREFIX/lib/libz.a --enable-mpi
+./configure --prefix=$PREFIX LDFLAGS=$BUILD_PREFIX/lib/libz.a --enable-mpi CC=mpicc CXX=mpicxx
 make -j 4
 make install
 
